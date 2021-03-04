a8a831aba995927c6c8b711a2120a06fe8ec54b6
Charles Magid <cmmagid@gmail.com>
2021-03-04 16:59:02 -0500

    hygiene

--- roman_numerals.rb
+++ roman_numerals.rb
@@ -25,7 +25,6 @@ require 'pry'
 # => [["I", "1"], ["V", "5"], ["X", "10"], ["L", "50"], ["C", "100"], ["D", "500"], ["M", "1000"]]
 # going from small to big => subtract
 # going grom big to small => add
-
 class RomanNumerals
   CORRESPONDING_ARABIC_NUMBERS = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
   KEY_ROMAN_NUMBERS = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
